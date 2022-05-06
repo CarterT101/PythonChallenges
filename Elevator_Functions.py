b5c41b39e05cc8110ccda4158e2920e908323559
Carter T <carter.thurman00@gmail.com>
2022-05-05 19:49:47 -0700

    More work on elevator project

--- Elevator_Functions.py
+++ Elevator_Functions.py
@@ -34,40 +34,79 @@ def create_people_list(n=10):
 
 
 """
-if the current_floor value is in the people_goto_list, that means there is someone who needs to get dropped off on the 
+if the current_floor value is in the people_go_to, that means there is someone who needs to get dropped off on the 
 current floor and this function will drop off the person from the list so they are no longer in the elevator
 """
 
-def going_up(current_floor, people_goto_list, total_floors):
-    elevator_population = 0
-    print("variable = " + str(people_goto_list[current_floor]))
+def going_up(current_floor, people_go_to, people_on_floor, total_floors, people_list, elevator_population):
+    print("Elevator currently going up...\n")
+    max_level = max(people_on_floor + people_go_to)  # finds the max level the elevator needs to go to
     go_up = True
     while go_up:
-        if current_floor == len(total_floors):  # if current floor is every the length of total_floors (which is 5)
-                                                # it should stop as there are no more floors to go up to
+        if current_floor == max_level:  # makes it so if there is no one on the top floor or wants to go to it, it won't
             go_up = False
-        if total_floors[current_floor] > 0:     # if the current_floor variable has people on it, '> 0',
+        if current_floor == 1:          # if current floor is one, pass this if statement
+            pass
+        else:               # if current floor is not one, meaning it is currently in the loop of going upwards, print
+            print("Going up to floor {}...\n".format(current_floor))
+        # prints current floor, elevator population, and the floor population of the current floor it is on,
+        # if the floor population is above 0, it means people want to get on the elevator
+        print("Current Floor: " + str(current_floor), "\nElevator Pop: " + str(elevator_population),
+              "\nFloor {} Population: ".format(current_floor) + str(total_floors[current_floor]))
+        print("")
+
+        if people_on_floor.count(current_floor) > 0:     # if the current_floor variable has people on it, ie: '> 0',
                                                 # it should continue with the next lines of code
             for person in range(total_floors[current_floor]):
-                if elevator_population == 5:
-                    print("Elevator at max capacity: {}".format(elevator_population))
+
+                if elevator_population == 5 and people_go_to.count(current_floor) == 0:
+                    print("1 Elevator at max capacity: {}".format(elevator_population))
                     break
                 elevator_population += 1
                 total_floors[current_floor] -= 1
-                print("People currently outside of elevator: " + str(total_floors[current_floor]))
+                print("One person getting on...")
+                if elevator_population > 5 and people_go_to.count(current_floor) > 0:
+                    pass
+                else:
+                    print("People currently inside elevator: " + str(elevator_population),
+                          "\tCurrent outside: " + str(total_floors[current_floor]))
+        print("")
+        if current_floor in people_go_to:
+            reply = True
+            for people in range(1, len(people_list)):
+                if people_list[people][0] <= current_floor and people_list[people][1] == current_floor:
+                    if reply:
+                        reply = False
+                    elevator_population -= 1
+                    people_go_to.remove(current_floor)
+                    print("One person getting off...")
+            if elevator_population >= 5 and total_floors[current_floor] != 0:
+                print("2 Elevator at max capacity: {}".format(elevator_population))
+                pass
+            else:
+                for person in range(total_floors[current_floor]):
+                    if elevator_population == 5:
+                        print("3 Elevator at max capacity: {}".format(elevator_population))
+                        pass
+                    else:
+                        elevator_population += 1
+                        total_floors[current_floor] -= 1
+                        print("One person getting on...")
+        print("")
         current_floor += 1
-        # WORK ON GETTING THE PEOPLE OUT OF THE ELEVATOR
-    pass
+    print("People currently inside elevator: " + str(elevator_population), "\tPeople on floors: " + str(total_floors))
+    going_down(current_floor, people_go_to, people_on_floor, total_floors, people_list, elevator_population)
 
 
-def going_down(current_floor, go_to_floor, people_goto_list):
+def going_down(current_floor, people_go_to, people_on_floor, total_floors, people_list, elevator_population):
+    print("Now going down...")
     go_down = True
     while go_down:
-        if current_floor == go_to_floor:
-            break
+        if current_floor < 1:
+            go_down = False
         current_floor -= 1
-        if current_floor in people_goto_list:
-            for i in people_goto_list:
+        if current_floor in people_go_to:
+            for i in people_go_to:
                 if i == current_floor:
                     pass
         break
--- Elevator_Project.py
+++ Elevator_Project.py
@@ -1,15 +1,17 @@
-import random
 import Elevator_Functions as Ef
 
-max_capacity = 5        # maximum capacity of elevator
+# max_capacity of elevator is 5
 current_floor = 1       # starting current floor should be level 1
+elevator_population = 0
 total_floors = {
     1: 0,
     2: 0,
     3: 0,
     4: 0,
     5: 0
-}        # total floors should be 5 as they will be 5 floors, and the value being the amount of people on it
+}
+# total floors should be 5 as they will be 5 floors, and the value being the amount of people on it
+# should be set to 0 for now until list of people is created
 
 
 """
@@ -26,7 +28,6 @@ else:
 This section will allow users to customize the floor the current person is on and wants to go to, if at any point
 the user does not want to input data, they can hit enter key and values will already be created for them
 """
-
 
 line = input("Input the floor the first person is on and the floor they want to go to separated by ',':"
              "\nIf not just hit 'enter' key.\n")
@@ -49,22 +50,28 @@ print("")
 temp_person_list = {
     1: [1, 5],
     2: [1, 5],
-    3: [1, 3],
+    3: [1, 5],
     4: [1, 3],
-    5: [5, 2]
+    5: [1, 5],
+    6: [3, 2],
+    7: [5, 2]
 }
 
-floor_list_amount = []
+people_on_floor = []    # makes a list of people and what floor they are on
+people_go_to = []       # makes a list of what floor people want to go to
+floor_list_amount = []  # makes a list of how many people are on each floor, different from what floor they are on
 for s in temp_person_list.items():
-    floor_list_amount.append(s[1][0])
+    floor_list_amount.append(s[1][0])  # this makes sure all lists get added the correct values
+    people_go_to.append(s[1][1])
+    people_on_floor.append(s[1][0])
 
 for k in total_floors.keys():
-    total_floors[k] = floor_list_amount.count(k)
+    total_floors[k] = floor_list_amount.count(k)  # adds the total amount of people on each floor to the dictionary
 
 for floornumber, amountofpeople in total_floors.items():
-    print("Floor: " + str(floornumber) + " has " + str(amountofpeople) + " people")
+    print("Floor: " + str(floornumber) + " has " + str(amountofpeople) + " person(s)")
 
 print("")
 
-Ef.going_up(current_floor, temp_person_list, total_floors)
+Ef.going_up(current_floor, people_go_to, people_on_floor, total_floors, temp_person_list, elevator_population)
 
